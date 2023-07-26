dcd908836ba9d7c30bb53599ac03c4877afaa832
Loren Oh <loh@eoas.ubc.ca>
2023-07-25 22:55:05 -0700

    Initialize repository

---
+++ src/init_config.py
@@ -0,0 +1,25 @@
+import json
+
+from pathlib import Path
+from rich import print
+
+
+def init_config():
+    # Resolve file path for the project directory
+    prj_d = Path(__file__).absolute().parents[1]
+
+    print(f"Initializing {prj_d}/config.json...\n")
+
+    config = {
+        "prj_d": prj_d.as_posix(),
+        "case": "/Howard16TB/data/loh/CGILS_S6"
+    }
+
+    with open(prj_d / "config.json", "w") as json_file:
+        json.dump(config, json_file, indent=4)
+
+    print(config)
+
+
+if __name__ == "__main__":
+    init_config()
